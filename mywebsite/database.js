3d8a1c1de36cf484b424b31bcf37eac6e7361c32
wissem-sassi <sassiwissem97@gmail.com>
2021-05-06 11:14:36 +0200

    affichage dynamique des produit

--- mywebsite/database.js
+++ mywebsite/database.js
@@ -2,9 +2,9 @@ var mysql = require('mysql');
 
 var conn = mysql.createConnection({
   host: 'localhost', // assign your host name
-  user: 'phpmyadmin',      //  assign your database username
-  password: 'ayoub',      // assign your database password
-  database: 'phpmyadmin' // assign database Name
+  user: 'root',      //  assign your database username
+  password: '',      // assign your database password
+  database: 'aws' // assign database Name
 }); 
 conn.connect(function(err) {
   if (err) throw err;
--- mywebsite/routes/shop_route.js
+++ mywebsite/routes/shop_route.js
@@ -5,6 +5,53 @@ var mysql = require('mysql');
 var bcrypt = require('bcrypt');
 
 router.get('/shop', function(req, res, next) {
-    res.render('shop');
+	var produit = '';
+	var categorie = '';
+	if(req.query.categorie === undefined){
+     conn.query('SELECT * FROM products ORDER BY id desc',function(err,rows)     {
+ 
+        if(err) {
+            req.flash('error', err);
+            // render to views/books/index.ejs
+              
+        } else {
+            produit = rows;
+            conn.query('SELECT * FROM categorie ORDER BY id desc',function(err,rows)     {
+ 
+		        if(err) {
+		            req.flash('error', err);
+		            
+		              
+		        } else {
+		            categorie = rows;
+		            res.render('shop',{data:produit,categorie});
+		        }
+    		});
+    	}
+        
+    });
+  }else{
+  	conn.query('SELECT * FROM products where categorie='+req.query.categorie+' ORDER BY id desc',function(err,rows)     {
+ 
+        if(err) {
+            console.log('error', err);
+              
+        } else {
+            produit = rows;
+            conn.query('SELECT * FROM categorie ORDER BY id desc',function(err,rows)     {
+ 
+		        if(err) {
+		            req.flash('error', err);
+		            // render to views/books/index.ejs
+		              
+		        } else {
+		            categorie = rows;
+		            res.render('shop',{data:produit,categorie});
+		        }
+    		});
+    	}
+        
+    });
+  }
   });
   module.exports = router;
