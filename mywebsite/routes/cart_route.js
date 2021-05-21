d6427fbc1e8ee57d03ae68f9cc1604c8bd572abb
uvsq22006828 <aichatasisibe36@gmail.com>
2021-05-21 04:36:00 +0200

    affichage des produits dans le panier

--- mywebsite/routes/cart_route.js
+++ mywebsite/routes/cart_route.js
@@ -5,6 +5,47 @@ var mysql = require('mysql');
 var bcrypt = require('bcrypt');
 
 router.get('/cart', function (req, res, next) {
-    res.render('cart');
+    var panier = '';
+    var produit = '';
+
+    conn.query('SELECT * FROM panier where idUser=' + 2, function (err, rows) {
+        if (err) {
+            console.log('error', err);
+
+        } else {
+            panier = rows;
+
+                    res.render('cart', { result: panier });
+                }
+            });
+
+            // supprimer un produit dans le panier
+            /*if (req.query.id === undefined) {
+                conn.query('SELECT * FROM panier where idUser=' + 2, function (err, rows) {
+                    if (err) {
+                        console.log('error', err);
+            
+                    } else {
+                        panier = rows;
+            
+                                res.render('cart', { result: panier });
+                            }
+                        });
+              } else {
+                conn.query('DELETE FROM panier WHERE idpanier=' + req.query.id,function (err, rows){
+                  
+                    if (err) {
+                        console.log('error', err);
+            
+                    } else {
+                        res.render('cart');
+                    }
+                });
+            
+                
+              } */       
+            
+            
+    
 });
 module.exports = router;
